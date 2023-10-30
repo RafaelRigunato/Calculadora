cb59823554d397377891d7284f0f0a21ef3bc139
RafaelRigunato <96690017+RafaelRigunato@users.noreply.github.com>
2023-10-30 00:17:05 -0300

    Update Usd_Spread.py

--- Usd_Spread.py
+++ Usd_Spread.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 def cotadora():
     url = "http://economia.awesomeapi.com.br/json/last/USD-BRL"
@@ -6,20 +7,38 @@ def cotadora():
     ask_value = float(taxa_cal.get('USDBRL', {}).get('ask'))
     return ask_value
 
-trashold = float(0.020)
+trashold = float(0.025)
+data = []
+data2 = []
 while True:
 
     taxa_oferecida = float(input("Digite a taxa oferecida: "))
+    
+    if taxa_oferecida == 191301:
+        break
+
     taxa_coletada = cotadora()
 
-    resultado = taxa_oferecida - taxa_coletada
-    print("Exemplos: 10BPS - ", taxa_coletada + 0.010, "15BPS - ", taxa_coletada + 0.015,"20BPS - ", taxa_coletada + 0.020)
-    print("a taxa coletada e:", taxa_coletada)
-    print("a taxa oferecida e:", taxa_oferecida)
-    print("O Spread e:", resultado)
+    spread = taxa_oferecida - taxa_coletada
 
-    if resultado > trashold:
-        print("Nao fecha")
+    #Simulacao
+
+    spread_10bps = taxa_coletada + 0.010
+    spread_15bps = taxa_coletada + 0.015
+    spread_20bps = taxa_coletada + 0.020
+    spread_25bps = taxa_coletada + 0.025
+    
+    data.append([spread_10bps, spread_15bps, spread_20bps, spread_25bps])
+
+    df = pd.DataFrame(data, columns=["10BPS", "15BPS", "20BPS", "25BPS"])
+    print(df)
+
+    data2.append([taxa_coletada, taxa_oferecida, spread])
+    df2 = pd.DataFrame(data2, columns=["Taxa Coletada", "Taxa Oferecida", "Spread"])
+    print(df2)
+
+    if spread > trashold:
+        print("SPREAD ACIMA, NÃO FECHAR")
         
     else:
-        print("pode fechar")
+        print("SPREAD ABAIXO, PODE FECHAR")
